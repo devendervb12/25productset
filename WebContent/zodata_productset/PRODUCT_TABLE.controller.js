7b47c1339a9d3a40fd9df305985991832d3dbd83
devendervb12 <info@s-max.in>
2017-10-23 21:01:03 +0530

    initial commit

---
+++ WebContent/zodata_productset/PRODUCT_TABLE.controller.js
@@ -0,0 +1,42 @@
+sap.ui.controller("zodata_productset.PRODUCT_TABLE", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf zodata_productset.PRODUCT_TABLE
+*/
+	onInit: function() {
+		
+		var serviceURL = "proxy/http/122.180.87.238:8000/sap/opu/odata/SAP/ZGW_BATCH25_PRODUCT_SRV/";
+		var oModel = new sap.ui.model.odata.v2.ODataModel(serviceURL);
+        
+		this.getView().setModel(oModel);
+	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf zodata_productset.PRODUCT_TABLE
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf zodata_productset.PRODUCT_TABLE
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf zodata_productset.PRODUCT_TABLE
+*/
+//	onExit: function() {
+//
+//	}
+
+});
